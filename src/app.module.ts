e08b689765207da3498ab6ac7d3eb26f771d98b7
wesleygf <wesleygfsilva@hotmail.com>
2026-01-28 10:02:19 -0300

    adicionando biblioteca postgress

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,11 @@
 import { Module } from '@nestjs/common';
-import { Carona } from './carona/entities/carona.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CaronaModule } from './carona/carona.module';
-import { Usuario } from './usuario/entities/usuario.entity';
-import { Categoria } from './categoria/entities/categoria.entity';
 import { UsuarioModule } from './usuario/usuario.module';
 import { CategoriaModule } from './categoria/categoria.module';
 import { AppController } from './app.controller';
 import { ProdService } from './data/services/prod.service';
 import { ConfigModule } from '@nestjs/config';
-import { DevService } from './data/services/dev.service';
 
 @Module({
   imports: [
